0ad3a64090f2ec13216097e0e27fd058f18ede45
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2020-02-11 19:12:40 +0300

    Added profile image

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.red,
+              backgroundImage: AssetImage('images/profile.png'),
+            ),
+            Text(
+              'Ahmed',
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: Colors.white
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
