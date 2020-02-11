91b42abc31e2ac4f3cdb8e73c723d75215a3ff41
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2020-02-11 22:29:27 +0300

    Centered items on column

--- lib/main.dart
+++ lib/main.dart
@@ -8,72 +8,75 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Column(
-          children: <Widget>[
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage('images/profile.png'),
-            ),
-            Text(
-              'Ahmed Tawfeeq',
-              style: TextStyle(
-                fontFamily: 'Pacifico',
-                fontSize: 40,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/profile.png'),
               ),
-            ),
-            Text(
-              'FLUTTER DEVELOPER',
-              style: TextStyle(
-                fontFamily: 'Source Sans Pro',
-                color: Colors.teal.shade100,
-                fontSize: 20,
-                letterSpacing: 2.5,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Card(
-              margin: EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 25,
-              ),
-              child: ListTile(
-                leading: Icon(
-                  Icons.phone,
-                  color: Colors.teal,
+              Text(
+                'Ahmed Tawfeeq',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
                 ),
-                title: Text(
-                  '+964 773 709 5696',
-                  style: TextStyle(
-                    fontFamily: 'Source Sans Pro',
-                    color: Colors.teal.shade900,
-                    fontSize: 20,
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal.shade100,
+                  fontSize: 20,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+964 773 709 5696',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      color: Colors.teal.shade900,
+                      fontSize: 20,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Card(
-              margin: EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 25,
-              ),
-              child: ListTile(
-                leading: Icon(
-                  Icons.web,
-                  color: Colors.teal,
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
                 ),
-                title: Text(
-                  'ahmedtawfeeq.me',
-                  style: TextStyle(
-                    fontFamily: 'Source Sans Pro',
-                    color: Colors.teal.shade900,
-                    fontSize: 20,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.web,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'ahmedtawfeeq.me',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      color: Colors.teal.shade900,
+                      fontSize: 20,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
